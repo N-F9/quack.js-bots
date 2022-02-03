12529b8d80e011aa03fce35d894591f03428b6dc
n-f9 <ndferguson@protonmail.com>
2022-02-03 09:32:22 -0500

    duckduckgo search

--- out/modules/duckduckgo.js
+++ out/modules/duckduckgo.js
@@ -1,3 +1,38 @@
+import { QuackJSUtils } from '@n-f9/quack.js';
 export default (Quack) => {
-    console.log('');
+    Quack.CreateCommand({
+        name: 'search',
+        description: 'Search a specific query.',
+        guilds: ['936423188005531688'],
+        permission: 'everyone',
+        options: [
+            {
+                name: 'query',
+                type: 'STRING',
+                description: 'The query which will be search.',
+                required: true,
+            },
+        ],
+        async execute(interation) {
+            const query = interation.options.getString('query');
+            if (!query)
+                return interation.reply(QuackJSUtils.Discord.Embed({
+                    embeds: [
+                        {
+                            title: 'An Error Occurred',
+                            color: '#FFD300',
+                        },
+                    ],
+                }));
+            interation.reply(QuackJSUtils.Discord.Embed({
+                embeds: [
+                    {
+                        title: 'DuckDuckGo Search',
+                        color: '#FFD300',
+                        description: `[${query}](https://duckduckgo.com/?q=${encodeURIComponent(query)})`,
+                    },
+                ],
+            }));
+        },
+    });
 };
--- src/modules/duckduckgo.ts
+++ src/modules/duckduckgo.ts
@@ -2,5 +2,45 @@ import { QuackJS, QuackJSUtils } from '@n-f9/quack.js'
 import * as DiscordJS from 'discord.js'
 
 export default (Quack: QuackJS) => {
-	console.log('')
+	Quack.CreateCommand({
+		name: 'search',
+		description: 'Search a specific query.',
+		guilds: ['936423188005531688'],
+		permission: 'everyone',
+		options: [
+			{
+				name: 'query',
+				type: 'STRING',
+				description: 'The query which will be search.',
+				required: true,
+			},
+		],
+		async execute(interation: DiscordJS.CommandInteraction) {
+			const query = interation.options.getString('query')
+
+			if (!query)
+				return interation.reply(
+					QuackJSUtils.Discord.Embed({
+						embeds: [
+							{
+								title: 'An Error Occurred',
+								color: '#FFD300',
+							},
+						],
+					}),
+				)
+
+			interation.reply(
+				QuackJSUtils.Discord.Embed({
+					embeds: [
+						{
+							title: 'DuckDuckGo Search',
+							color: '#FFD300',
+							description: `[${query}](https://duckduckgo.com/?q=${encodeURIComponent(query)})`,
+						},
+					],
+				}),
+			)
+		},
+	})
 }
