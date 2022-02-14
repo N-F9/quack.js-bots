0509ee41de87a4794ab348041780ed32fe73ff83
n-f9 <ndferguson@protonmail.com>
2022-02-14 13:38:10 +0000

    ducky init

---
+++ ducky/out/index.js
@@ -0,0 +1,16 @@
+import 'dotenv/config';
+import { QuackJS, QuackJSUtils } from '@n-f9/quack.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const Quack = new QuackJS(process.env.TOKEN, {});
+const files = QuackJSUtils.GetFiles('./out/modules');
+const getModules = async () => {
+    for (const file of files) {
+        const execute = (await import(path.join(__dirname, '../', file))).default;
+        await execute(Quack);
+    }
+};
+await getModules();
+await Quack.Start(Quack);
